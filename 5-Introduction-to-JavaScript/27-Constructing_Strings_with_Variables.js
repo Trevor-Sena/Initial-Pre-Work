d85d9819cdc7d141123a3a9ddfeaeadfa135459a
trevor-sena <trevor.sena@gmail.com>
2019-08-02 13:48:07 -0600

    Set myName and build myStr with myName between the strings "My name is " and " and I am well!"

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,9 @@
+// By using the concatenation operator (+), you can insert one or more variables into a string you're building.
+
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Trevor";
+var myStr = "My name is " + myName + " and I am well!";

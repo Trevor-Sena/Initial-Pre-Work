86cd861db34baf58d96164398834992923977540
trevor-sena <trevor.sena@gmail.com>
2019-08-01 20:53:37 -0600

    Create two new string variables: myFirstName and myLastName.

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,10 @@
+// "your name" is called a string literal. It is a string because it is a series of zero or more characters enclosed in single or double quotes.
+
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+
+var myFirstName = "First Name";
+var myLastName = "Last Name";

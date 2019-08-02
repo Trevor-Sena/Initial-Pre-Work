b6b07cedbb480c0bc9788aef90274fa86697e16a
trevor-sena <trevor.sena@gmail.com>
2019-08-01 19:42:02 -0600

    Create a variable called myName.

---
+++ 5-Introduction-to-JavaScript/2-Declare-JavaScript-Variables.js
@@ -0,0 +1,7 @@
+// Variables allow computers to store and manipulate data in a dynamic fashion. They do this by using a "label" to point to the data rather than using the data itself. JavaScript provides seven different data types which are undefined, null, boolean, string, symbol, number, and object.
+
+// Example
+var ourName;
+
+// Declare myName below this line
+var myName;

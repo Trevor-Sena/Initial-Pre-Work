0abab97ec3138d16f30b8d3e9ff985bd36a3cdfa
trevor-sena <trevor.sena@gmail.com>
2019-08-01 20:34:23 -0600

    Set remainder equal to the remainder of 11 divided by 3 using the remainder (%) operator.

---
+++ 5-Introduction-to-JavaScript/16-Finding-a-Remainder-in-JavaScript.js
@@ -0,0 +1,5 @@
+// The remainder operator % gives the remainder of the division of two numbers. The remainder operator is sometimes incorrectly referred to as the "modulus" operator. It is very similar to modulus, but does not work properly with negative numbers.
+
+// Only change code below this line
+
+var remainder = 11 % 3;

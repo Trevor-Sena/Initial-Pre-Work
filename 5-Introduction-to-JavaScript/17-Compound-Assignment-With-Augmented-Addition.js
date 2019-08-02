a4e86313e15201803e5e93131924c465d5a12d68
trevor-sena <trevor.sena@gmail.com>
2019-08-01 20:45:04 -0600

    Convert the assignments for a, b, and c to use the += operator.

---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,11 @@
+// It is common to use assignments to modify the contents of a variable, there are operators which do both a mathematical operation and assignment in one step. One such operator is the += operator.
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a += 12;
+b += 9;
+c += 7;

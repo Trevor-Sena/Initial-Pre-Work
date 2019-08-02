1505e2de4ecb8f92aecc64ed827acddcd84bd717
trevor-sena <trevor.sena@gmail.com>
2019-08-02 13:42:41 -0600

    Build myStr over several lines by concatenating these two strings: "This is the first sentence. " and "This is the second sentence."

---
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -0,0 +1,10 @@
+// To concatenate a string onto the end of an existing string variable use the += operator. This is helpful to break a long string over several lines.
+
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";

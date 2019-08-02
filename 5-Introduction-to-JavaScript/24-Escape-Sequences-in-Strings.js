fa3447f6a4d0f1634bee2958039258d39308644c
trevor-sena <trevor.sena@gmail.com>
2019-08-02 13:27:56 -0600

    Add escape sequences to insert special characters correctly.

---
+++ 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
@@ -0,0 +1,3 @@
+// Quotes are not the only characters that can be escaped inside a string. You can use escaped on characters you might not otherwise be able to type out or to represent multiple quotes in a string without JavaScript misinterpreting what you mean.
+
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine";

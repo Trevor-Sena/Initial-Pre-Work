8239c0949cf7c9f9df5c1e6534813461602e58e2
trevor-sena <trevor.sena@gmail.com>
2019-08-02 13:35:08 -0600

    Concatenate myStr from the strings "This is the start. " and "This is the end."

---
+++ 5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
@@ -0,0 +1,8 @@
+// You can build a new string out of other strings by concatenating them together by using the + operator.
+
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start. " + "This is the end.";

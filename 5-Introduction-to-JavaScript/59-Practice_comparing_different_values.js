7bac77e0074cac9e8193935608981b61b6a32e6e
trevor-sena <trevor.sena@gmail.com>
2019-08-04 14:09:07 -0600

    Modify the function so that it returns "Equal" only when the values are strictly equal.

---
+++ 5-Introduction-to-JavaScript/59-Practice_comparing_different_values.js
@@ -0,0 +1,12 @@
+// Practiced using the equality operator (==) and the strict equality operator (===).
+
+// Setup
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+compareEquality(10, "10");

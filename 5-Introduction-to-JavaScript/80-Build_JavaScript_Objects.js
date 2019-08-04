bbd087b560628231f419f2e6ab15a9bbe9d2ce49
trevor-sena <trevor.sena@gmail.com>
2019-08-04 17:58:24 -0600

    Create an object called myDog which containing properties "name" (a string), "legs", "tails" and "friends".

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,21 @@
+// Objects are useful for storing data in a structured way and you access the data in objects through what are called properties.
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Spot",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["none"]
+
+
+
+};

440e1be3734e9afb7010996e8f6a62b0fa2d8962
trevor-sena <trevor.sena@gmail.com>
2019-08-04 12:35:54 -0600

    Create a shopping list in the variable myList.

---
+++ 5-Introduction-to-JavaScript/45-Shopping_List.js
@@ -0,0 +1,8 @@
+// Learned how to create a shopping list using a multi-dimensional array containing several sub-arrays.
+var myList = [
+    ["Butter",1],
+    ["Milk",2],
+    ["Cheese",3],
+    ["Pop",5],
+    ["Bread",3],
+];

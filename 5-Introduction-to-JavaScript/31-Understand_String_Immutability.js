57d859a8bc22dcbec57cdee0e39e18ba4de40273
trevor-sena <trevor.sena@gmail.com>
2019-08-04 10:56:14 -0600

    Correct the assignment to myStr so it contains the string value of Hello World.

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,8 @@
+// String values are immutable, which means that they cannot be altered once created. This does not mean they cannot be changed, just that the individual characters of a string literal cannot be changed.
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me

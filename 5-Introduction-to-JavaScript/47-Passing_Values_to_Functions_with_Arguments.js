507aefa12d337b781d1556507543bcddc1134753
trevor-sena <trevor.sena@gmail.com>
2019-08-04 12:46:14 -0600

    Create functionWithArgs that accepts two arguments and outputs their sum to the dev console.

---
+++ 5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
@@ -0,0 +1,15 @@
+// When a function is defined, it is typically defined along with one or more parameters. The actual values that are passed into a function when it is called are known as arguments.
+
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+
+function functionWithArgs(a, b) {
+  console.log(a+b);
+}
+
+functionWithArgs(4, 4);

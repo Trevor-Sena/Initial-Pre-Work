3ecf2906ac809b42bad17712a4e85eed5343b054
trevor-sena <trevor.sena@gmail.com>
2019-08-04 12:40:40 -0600

    Create reusableFunction which prints "Hi World" to the dev console.

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,15 @@
+// Functions allow you to divide up your code into reusable parts.
+
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction() {
+    console.log("Hi World");
+}
+
+reusableFunction();

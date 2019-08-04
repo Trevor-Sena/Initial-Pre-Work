674071f6b6cb0dcd486732975b9a51be8e2400b5
trevor-sena <trevor.sena@gmail.com>
2019-08-04 12:06:46 -0600

    Create a variable called myData and set it to equal the first value of myArray.

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,11 @@
+// Using indexes you can access the data inside arrays and like strings, arrays use zero-based indexing.
+
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line.
+var myData = myArray[0];

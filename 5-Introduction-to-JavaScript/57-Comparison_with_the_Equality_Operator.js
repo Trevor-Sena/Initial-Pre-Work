7c28b169a8d7699bc493fefa85dad612367418b5
trevor-sena <trevor.sena@gmail.com>
2019-08-04 14:02:59 -0600

    Add the equality operator so that the function will return "Equal" when val is equivalent to 12.

---
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -0,0 +1,12 @@
+// Comparison Operators return a boolean true or false value. The equality operator compares two values and returns true if they're equivalent or false if they are not and is the most basic.
+
+// Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(10);

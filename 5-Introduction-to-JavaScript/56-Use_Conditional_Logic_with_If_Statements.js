c57fe38755369b2d181f81cfbed55687fb6f95a6
trevor-sena <trevor.sena@gmail.com>
2019-08-04 13:58:13 -0600

    Create an if statement inside the function to return "Yes, that was true" if the parameter wasThatTrue is true and return "No, that was false" otherwise.

---
+++ 5-Introduction-to-JavaScript/56-Use_Conditional_Logic_with_If_Statements.js
@@ -0,0 +1,25 @@
+// In JavaScrip, If statements are used to make decisions in code. These conditions are known as Boolean conditions and they may only be true or false.
+
+// Example
+function ourTrueOrFalse(isItTrue) {
+  if (isItTrue) {
+    return "Yes, it's true";
+  }
+  return "No, it's false";
+}
+
+// Setup
+function trueOrFalse(wasThatTrue) {
+
+  // Only change code below this line.
+
+if (wasThatTrue) {
+    return "Yes, that was true";
+}
+  return "No, that was false";
+  // Only change code above this line.
+
+}
+
+// Change this value to test
+trueOrFalse(false);

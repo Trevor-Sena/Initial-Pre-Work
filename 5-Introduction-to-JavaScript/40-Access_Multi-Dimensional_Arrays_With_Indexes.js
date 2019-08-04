1a071b384ac1dfe0fdbcc95e6266e3fdbeefbdfe
trevor-sena <trevor.sena@gmail.com>
2019-08-04 12:15:15 -0600

    Select an element from myArray such that myData is equal to 8.

---
+++ 5-Introduction-to-JavaScript/40-Access_Multi-Dimensional_Arrays_With_Indexes.js
@@ -0,0 +1,7 @@
+// A multi-dimensional array, is as an array of arrays. The first set of brackets refers to the entries in the first level array and each additional pair of brackets refers to the next level of entries inside.
+
+// Setup
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+// Only change code below this line.
+var myData = myArray[2][1];

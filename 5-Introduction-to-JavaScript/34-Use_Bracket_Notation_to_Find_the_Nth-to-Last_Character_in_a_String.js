a2db88fe9e748e400bad6f45fce150c7e59dfb9d
trevor-sena <trevor.sena@gmail.com>
2019-08-04 11:06:50 -0600

    Find the second-to-last character in the lastName string.

---
+++ 5-Introduction-to-JavaScript/34-Use_Bracket_Notation_to_Find_the_Nth-to-Last_Character_in_a_String.js
@@ -0,0 +1,11 @@
+// To retrieve the Nth-to-last character us bracket notation.
+
+// Example
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2];

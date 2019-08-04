b642180c9313547991d3e05d6ab1063137505e46
trevor-sena <trevor.sena@gmail.com>
2019-08-04 11:55:27 -0600

    Modify myArray so that it contains both a string and a number.

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,7 @@
+// Using JavaScript array variables, we can store several pieces of data in one place.
+
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["Billy", 20];

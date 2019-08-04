a8bc4caeda43d678262f484cfe08901bf3d4db3b
trevor-sena <trevor.sena@gmail.com>
2019-08-04 15:47:58 -0600

    Remove the if/else statements.

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,9 @@
+// Using the Comparison with the Equality Operator all comparison operators return a boolean true or false value. This will eliminate the need to use if/else statement to do a comparison.
+
+function isLess(a, b) {
+  // Fix this code
+  return a <= b;
+}
+
+// Change these values to test
+isLess(10, 15);

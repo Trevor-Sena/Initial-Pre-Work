30e4811d2338fbf2e688a16e211c7ce00f12b09e
trevor-sena <trevor.sena@gmail.com>
2019-08-04 13:29:54 -0600

    Modify the welcomeToBooleans function so that it returns true when the run button is clicked.

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,10 @@
+// Booleans may only be one of two values: true or false and they are never written with quotes.
+
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}

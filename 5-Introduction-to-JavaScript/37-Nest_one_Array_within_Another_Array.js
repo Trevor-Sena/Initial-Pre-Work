4a32f39afcc82fabc04609e62153f60e984572d1
trevor-sena <trevor.sena@gmail.com>
2019-08-04 12:00:01 -0600

    Create a nested array called myArray.

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,7 @@
+// A Multi-dimensional Array is when you nest arrays within other arrays.
+
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["Rockies", 3], ["Giants", 0]];

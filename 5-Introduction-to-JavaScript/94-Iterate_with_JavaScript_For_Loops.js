35960b493b494ef32020e75ccafb65b6f243be24
trevor-sena <trevor.sena@gmail.com>
2019-08-04 20:35:57 -0600

    Push the values 1 through 5 onto myArray using a for loop.

---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,16 @@
+// The most common type of JavaScript loop is called a "for loop" because it runs "for" a specific number of times. For loops are declared with three optional expressions: for ([initialization]; [condition]; [final-expression]).
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 1; i < 6; i++) {
+  myArray.push(i);
+}

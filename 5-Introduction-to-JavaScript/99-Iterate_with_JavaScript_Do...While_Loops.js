57c51fae776530333d9162d3fa192442b4e2322b
trevor-sena <trevor.sena@gmail.com>
2019-08-04 21:10:34 -0600

    Change the while loop in the code to a "do...while" loop and will push the number 10 to myArray, and i will be equal to 11 whe code finishes running.

---
+++ 5-Introduction-to-JavaScript/99-Iterate_with_JavaScript_Do...While_Loops.js
@@ -0,0 +1,12 @@
+// A "do...while" loop will "do" one pass of the code inside the loop no matter what, and then it run "while" a specified condition is true and stops once that condition is no longer true.
+
+// Setup
+var myArray = [];
+var i = 10;
+
+// Only change code below this line.
+do {
+  myArray.push(i);
+  i++
+}
+while (i < 10);

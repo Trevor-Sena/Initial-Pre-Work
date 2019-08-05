f75b08b0a91de6795e1643b793ba7d5979207463
trevor-sena <trevor.sena@gmail.com>
2019-08-04 20:44:07 -0600

    Push the odd numbers from 9 through 1 to myArray using a for loop.

--- 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
+++ 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
@@ -1,1 +1,16 @@
+// A for loop can also count backwards. In order to count backwards you need to change our initialization, condition, and final-expression.
 
+// Example
+var ourArray = [];
+
+for (var i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 9; i > 0; i -= 2) {
+  myArray.push(i);
+}

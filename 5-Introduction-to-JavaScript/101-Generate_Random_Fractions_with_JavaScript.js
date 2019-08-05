e7c2384616134cdf8a601a2952577017536e215f
trevor-sena <trevor.sena@gmail.com>
2019-08-04 21:21:11 -0600

    Change randomFraction to return a random number instead of returning 0.

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,10 @@
+// A Math.random() function generates a random decimal number between 0 (inclusive) and not quite up to 1 (exclusive).
+
+function randomFraction() {
+
+  // Only change code below this line.
+
+  return Math.random();
+
+  // Only change code above this line.
+}

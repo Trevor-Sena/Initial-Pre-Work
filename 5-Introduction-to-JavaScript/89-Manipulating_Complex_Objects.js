1668c409310ef7e662838607b24fb54e3db3a1ad
trevor-sena <trevor.sena@gmail.com>
2019-08-04 19:53:53 -0600

    Add a new album to the myMusic array. Add artist and title strings, release_year number, and a formats array of strings.

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,26 @@
+// A JavaScript object is one way to handle flexible data. They allow for arbitrary combinations of strings, numbers, booleans, arrays, functions, and objects.
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add record here
+  {
+    "artist": "Beastie Boys",
+    "title": "Licensed to Ill",
+    "release_year": 1986,
+    "formats": [
+      "CD",
+      "Cassette",
+      "LP"
+    ],
+  }
+];

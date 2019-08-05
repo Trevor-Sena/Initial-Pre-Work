3402cddc62c4a7d07b149852682d96db3d733960
trevor-sena <trevor.sena@gmail.com>
2019-08-04 22:08:55 -0600

    Add a multiple conditional operator in the checkSign function to check if a number is positive, negative or zero.

---
+++ 5-Introduction-to-JavaScript/107-Use_Multiple_Conditional_(Ternary)_Operators.js
@@ -0,0 +1,7 @@
+// Multiple Conditional (Ternary) Operators check for multiple conditions.
+
+function checkSign(num) {
+  return (num == 0) ? "zero" : (num < 0) ? "negative" : "positive";
+}
+
+checkSign(10);

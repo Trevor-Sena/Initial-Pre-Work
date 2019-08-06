1bb10954121e5ec1593f4d6ddeb2034d87549f9c
trevor-sena <trevor.sena@gmail.com>
2019-08-05 21:47:25 -0600

    Import the default export, subtract, from the file "math_functions".

---
+++ 6-Introduction-to-the-ES6-Challenges/26-Import-a-Default-Export.js
@@ -0,0 +1,5 @@
+// To import a default export, you need to use a different import syntax. The primary method of importing a default export is to simply write the value's name after import.
+
+"use strict";
+subtract(7,4);
+import subtract from "math_functions";

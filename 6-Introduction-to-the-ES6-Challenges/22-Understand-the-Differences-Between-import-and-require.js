ea5103ead67ccbf572a12579cac739dcd0bf407f
trevor-sena <trevor.sena@gmail.com>
2019-08-05 21:29:03 -0600

    Add an import statement that will allow the current file to use the capitalizeString function.

---
+++ 6-Introduction-to-the-ES6-Challenges/22-Understand-the-Differences-Between-import-and-require.js
@@ -0,0 +1,5 @@
+// Import will let you choose which parts of a module or file to load into a given file, saving time and memory.
+
+"use strict";
+capitalizeString("hello!");
+import { capitalizeString } from "string_functions"

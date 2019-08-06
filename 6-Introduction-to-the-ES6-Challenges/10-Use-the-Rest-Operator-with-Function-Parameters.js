4e87878e6784ccf8f3de4d0ebe44b604b69d4499
trevor-sena <trevor.sena@gmail.com>
2019-08-05 19:04:20 -0600

    Modify the function sum so that it uses the rest operator and it works in the same way with any number of parameters.

---
+++ 6-Introduction-to-the-ES6-Challenges/10-Use-the-Rest-Operator-with-Function-Parameters.js
@@ -0,0 +1,9 @@
+// With the rest operator, you can create functions that take a variable number of arguments. These arguments are stored in an array that can be accessed later from inside the function.
+
+const sum = (function() {
+  "use strict";
+  return function sum(...args) {
+    return args.reduce((a, b) => a + b, 0);
+  };
+})();
+console.log(sum(1, 2, 3)); // 6

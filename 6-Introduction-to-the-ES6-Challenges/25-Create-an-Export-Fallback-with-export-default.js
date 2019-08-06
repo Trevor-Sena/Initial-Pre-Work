0f3ee55cfe34538418da583362926c8d738586a8
trevor-sena <trevor.sena@gmail.com>
2019-08-05 21:43:43 -0600

    Create a fallback value for the module.

---
+++ 6-Introduction-to-the-ES6-Challenges/25-Create-an-Export-Fallback-with-export-default.js
@@ -0,0 +1,4 @@
+// Export default will only export one value from a file. It is also used to create a fallback value for a file or module. You cannot use export default with var, let, or const.
+
+"use strict";
+export default function subtract(x,y) {return x - y;}

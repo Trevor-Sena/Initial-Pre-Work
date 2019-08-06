d90862e2f640b148a1664c7e5f5204c6dac5b061
trevor-sena <trevor.sena@gmail.com>
2019-08-05 18:40:55 -0600

    Rewrite the function assigned to the variable magic which returns a new Date() to use arrow function syntax.

---
+++ 6-Introduction-to-the-ES6-Challenges/6-Use-Arrow-Functions-to-Write-Concise-Anonymous-Functions.js
@@ -0,0 +1,3 @@
+// Inline functions don't need to be named because we do not reuse them anywhere else. When there is no function body, and only a return value, arrow function syntax allows you to omit the keyword return as well as the brackets surrounding the code.
+
+const magic = () => new Date();

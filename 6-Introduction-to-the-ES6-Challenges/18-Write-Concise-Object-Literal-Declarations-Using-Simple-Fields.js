f0574cfab12c9d76c730b668a018e4b6a8543e13
trevor-sena <trevor.sena@gmail.com>
2019-08-05 21:07:57 -0600

    Create and return a Person object.

---
+++ 6-Introduction-to-the-ES6-Challenges/18-Write-Concise-Object-Literal-Declarations-Using-Simple-Fields.js
@@ -0,0 +1,12 @@
+// Simple fields eliminates the redundancy of having to write two fields.
+const createPerson = (name, age, gender) => {
+  "use strict";
+  // change code below this line
+  return {
+    name,
+    age,
+    gender
+  };
+  // change code above this line
+};
+console.log(createPerson("Zodiac Hasbro", 56, "male")); // returns a proper object

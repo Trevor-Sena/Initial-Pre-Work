63f9ba22be757d78b1aa22efe97cc1fcac2888f5
trevor-sena <trevor.sena@gmail.com>
2019-08-05 21:16:26 -0600

    Write a proper constructor to create the Vegetable class.

---
+++ 6-Introduction-to-the-ES6-Challenges/20-Use-class-Syntax-to-Define-a-Constructor-Function.js
@@ -0,0 +1,15 @@
+// ES6 provides a new syntax to help create objects, using the keyword class. The class syntax is just a syntax, and not a full-fledged class based implementation of object oriented paradigm.
+function makeClass() {
+  "use strict";
+  /* Alter code below this line */
+  class Vegetable {
+    constructor(name){
+      this.name = name;
+    }
+  }
+  /* Alter code above this line */
+  return Vegetable;
+}
+const Vegetable = makeClass();
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // => should be 'carrot'
